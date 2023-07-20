5b0861bbabccaf7d8b818b46e36ed63678d4c95a
Dylan <yourmom12734@gmail.com>
2023-07-19 22:36:17 -0400

    dice roll lab

---
+++ Dice_roll_lab/Dice_roll_lab/Program.cs
@@ -0,0 +1,43 @@
+﻿using Dice_roll_lab;
+bool doContinue = true;
+while (doContinue)
+{
+    Console.WriteLine("How many sides are on the dice you would like to roll?");
+    string answer = Console.ReadLine();
+    Console.WriteLine("Roll the dice");
+    int answerNum = 0;
+    int.TryParse(answer, out answerNum);
+    int d1 = TheGuts.rng(answerNum);
+    int d2 = TheGuts.rng(answerNum);
+    Console.WriteLine($"The result for die 1 is :{d1}");
+    Console.WriteLine($"The result for die 2 is :{d2}");
+    int total = d1 + d2;
+
+
+    if ((answerNum == 6 && total == 7) || (answerNum == 6 && total == 11))
+    {
+        Console.WriteLine("WIN!!!!");
+    }
+    else if ((answerNum == 6 && total == 2) || (answerNum == 6 && total == 3) || (answerNum == 6 && total == 12))
+    {
+        Console.WriteLine("Craps");
+    }
+    if (answerNum == 6 && d1 == 1 && d2 == 1)
+    {
+        Console.WriteLine("Snake Eyes");
+    }
+    else if ((answerNum == 6 && d1 == 1 && d2 == 2) || (answerNum == 6 && d1 == 2 && d2 == 1))
+    {
+        Console.WriteLine("Ace Deuce");
+    }
+    else if (answerNum == 6 && d1 == 6 && d2 == 6)
+    {
+        Console.WriteLine("Box Cars");
+    }
+    Console.WriteLine("Do you wish to continue? (y/n)");
+    string userInput = Console.ReadLine();
+    if (userInput == "n")
+    {
+        break;
+    }
+}
---
+++ Dice_roll_lab/Dice_roll_lab/TheGuts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_roll_lab
+{
+    public class TheGuts
+    {
+        public static int rng(int i)
+        {
+            Random random = new Random();
+            return random.Next(1, i + 1);
+        }
+
+    }
+}
