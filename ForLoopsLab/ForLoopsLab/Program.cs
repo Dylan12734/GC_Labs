274d9e6b4ef4c2703bb804ffe1c5602436256b1c
Dylan <yourmom12734@gmail.com>
2023-07-26 21:28:26 -0400

    forLoops

---
+++ ForLoopsLab/ForLoopsLab/Program.cs
@@ -0,0 +1,22 @@
+﻿bool doContinue = true;
+while (doContinue)
+{
+    Console.WriteLine("Enter a number:");
+    string userinput = Console.ReadLine();
+    int input = 0;
+    int.TryParse(userinput, out input);
+    int final = 0;
+    for(int i = 0; i<input; i++)
+    {
+         final = final + i;
+    }
+    Console.WriteLine(final);
+    Console.WriteLine("Would you like to continue? (y/n)");
+    string uI = Console.ReadLine();
+    if (uI == "n")
+    {
+        Console.WriteLine("Goodbye");
+        break;
+    }
+    
+}
