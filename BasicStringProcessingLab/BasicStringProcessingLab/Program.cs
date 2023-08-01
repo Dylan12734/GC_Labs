d235a0c99d029a2c882e0b276f72ac74d67f74eb
Dylan <yourmom12734@gmail.com>
2023-07-31 21:06:55 -0400

    added basic string processing lab

---
+++ BasicStringProcessingLab/BasicStringProcessingLab/Program.cs
@@ -0,0 +1,29 @@
+﻿bool doContinue = true;
+while(doContinue)
+{
+    Console.WriteLine("Enter a sentence:");
+    string input = Console.ReadLine();
+    string[] splitSentence = input.Split(" ");
+    foreach(string s in splitSentence)
+    {
+        Console.WriteLine(s);
+    }
+    Console.WriteLine("would you like to continue? (y/n)");
+    string userinput = Console.ReadLine();
+    if (userinput == "n") break;
+
+}
+bool doContinue2 = true;
+List<string> words = new List<string>();
+while(doContinue2)
+{
+    Console.WriteLine("enter some text");
+    words.Add(Console.ReadLine());
+    foreach(string s in words)
+    {
+        Console.WriteLine(s);
+    }
+    Console.WriteLine("would you like to continue? (y/n)");
+    string input = Console.ReadLine();
+    if(input == "n") break;
+}
