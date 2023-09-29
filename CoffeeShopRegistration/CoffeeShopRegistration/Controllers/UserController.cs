b3e15e5be7e1d9b66056b8dd8e72526b1e8838df
Dylan <yourmom12734@gmail.com>
2023-09-28 22:15:42 -0400

    added coffee shop and personal website labs\

---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using CoffeeShopRegistration.Models;
+using CoffeeShopRegistration.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegistration.Controllers;
+
+public class UserController : Controller
+{
+    private readonly AppDbContext _appDpContext;
+    public UserController(AppDbContext appDpContext)
+    {
+        _appDpContext = appDpContext;
+    }
+    public IActionResult Index()
+    {
+        User user = new User();
+        return View();
+    }
+    [HttpPost]
+
+    public IActionResult ProcessUserForm([FromForm] User newUser)
+    {
+        if (!ModelState.IsValid)
+        {
+            return RedirectToAction("Index");
+        }
+        _appDpContext.Users.Add( newUser);
+        _appDpContext.SaveChanges();
+
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ CoffeeShopRegistration/CoffeeShopRegistration/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoffeeShopRegistration.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    [Column(TypeName = "nvarchar(50)")]
+    [Required]
+    public string UserName { get; set; }
+    [Column(TypeName = "nvarchar(50)")]
+    [Required]
+    public string Password { get; set; }
+    [Column(TypeName = "nvarchar(50)")]
+    [Required]
+    public string FirstName { get; set; }
+    [Column(TypeName = "nvarchar(50)")]
+    [Required]
+    public string LastName { get; set; }
+    [Column(TypeName = "nvarchar(50)")]
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
